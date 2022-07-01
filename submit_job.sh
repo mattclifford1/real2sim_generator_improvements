d5247fedf7f70067d418500d1242365ca581387f
Matt Clifford <mc15445@my.bristol.ac.uk>
2022-07-01 15:02:38 +0100

    pytroch load

--- submit_job.sh
+++ submit_job.sh
@@ -9,12 +9,13 @@
 # #SBATCH --gres=gpu:2
 # cd /user/home/mc15445/summer-project/real2sim_multitask
 
-module load languages/anaconda3/2021-3.9-bioconda
-module load libs/cuda/10.2-gcc-5.4.0-2.26
-module load tools/git/2.35.1
-
-source /mnt/storage/software/languages/anaconda/anaconda.3.9-2021.12-bioconda/etc/profile.d/conda.sh
-conda activate /user/work/mc15445/conda_envs/multi_task
+# module load languages/anaconda3/2021-3.9-bioconda
+# module load libs/cuda/10.2-gcc-5.4.0-2.26
+# module load tools/git/2.35.1
+#
+# source /mnt/storage/software/languages/anaconda/anaconda.3.9-2021.12-bioconda/etc/profile.d/conda.sh
+# conda activate /user/work/mc15445/conda_envs/multi_task
+module load languages/anaconda3/2021-3.8.8-cuda-11.1-pytorch
 echo 'Hello'
 srun python test.py
 srun python run_all.py --dir /user/work/mc15445/summer-project
