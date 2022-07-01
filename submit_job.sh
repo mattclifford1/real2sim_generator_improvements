a17319c65d8d0366ffed149266c9db6cea7f3461
Matt Clifford <mc15445@my.bristol.ac.uk>
2022-07-01 14:26:03 +0100

    modeules

--- submit_job.sh
+++ submit_job.sh
@@ -8,6 +8,11 @@
 
 # #SBATCH --gres=gpu:2
 # cd /user/home/mc15445/summer-project/real2sim_multitask
+
+module add languages/anaconda3/2021-3.9-bioconda
+module add libs/cuda/10.2-gcc-5.4.0-2.26
+module add tools/git/2.35.1
+
 srun conda activate /user/work/mc15445/conda_envs/multi_task
 srun python run_all.py --dir /user/work/mc15445/summer-project
 wait
