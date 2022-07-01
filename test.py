8c26e6a2b0bb00130172267e35ddd8d2b6be9231
Matt Clifford <mc15445@my.bristol.ac.uk>
2022-07-01 15:33:26 +0100

    no env

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 print('in python')
 import os
-print(os.environ['CONDA_DEFAULT_ENV'])
+# print(os.environ['CONDA_DEFAULT_ENV'])
 import torch
 print(torch.cuda.is_available())
 print(torch.cuda.mem_get_info())
