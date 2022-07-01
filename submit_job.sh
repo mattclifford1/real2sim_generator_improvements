18b5ace5c45fc101505623833b104c4b0a011aba
Matt Clifford <mc15445@my.bristol.ac.uk>
2022-07-01 14:48:43 +0100

    source

--- submit_job.sh
+++ submit_job.sh
@@ -12,6 +12,8 @@
 module load languages/anaconda3/2021-3.9-bioconda
 module load libs/cuda/10.2-gcc-5.4.0-2.26
 module load tools/git/2.35.1
+
+source /mnt/storage/software/languages/anaconda/anaconda.3.9-2021.12-bioconda/etc/profile.d/conda.sh
 conda activate /user/work/mc15445/conda_envs/multi_task
 
 srun python run_all.py --dir /user/work/mc15445/summer-project
