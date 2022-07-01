65790317e51933901114972fce86fa98946f63f7
Matt Clifford <mc15445@my.bristol.ac.uk>
2022-07-01 15:32:00 +0100

    othe rmoduels

--- submit_job.sh
+++ submit_job.sh
@@ -15,7 +15,11 @@
 #
 # source /mnt/storage/software/languages/anaconda/anaconda.3.9-2021.12-bioconda/etc/profile.d/conda.sh
 # conda activate /user/work/mc15445/conda_envs/multi_task
-module load languages/anaconda3/2019.10-3.7.4-tflow-2.1.0
+
+module load languages/anaconda3/2020-3.8.5
+module load libs/cudnn/10.1-cuda-10.0 
+module load libs/cuda/10.2-gcc-5.4.0-2.26
+# module load languages/anaconda3/2019.10-3.7.4-tflow-2.1.0
 echo 'Hello'
 srun python test.py
 srun python run_all.py --dir /user/work/mc15445/summer-project
