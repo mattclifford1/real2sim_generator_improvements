f99fd151a11fc8c7bd5eebb2981e9ee31d5b2ee5
matt clifford <matt.clifford@bristol.ac.uk>
2022-07-06 12:46:34 +0100

    layer plots

--- plotting/compare_weights.py
+++ plotting/compare_weights.py
@@ -44,10 +44,45 @@ def plot_avg_nets(networks, nets_weights, groupby):
             stacked=False,
             # title=ARGS.metric+' with Differing '+inner_groupby
             )
-    plt.ylabel('Weights abs difference')
+    plt.ylabel('Weights mean abs difference')
     plt.xticks(rotation=0)
     plt.legend(loc='right', title='Network')
     save_file = 'results/weights_diff_'+groupby+'.png'
+    plt.savefig(save_file)
+    print('Saved figure to: '+save_file)
+
+def plot_layers_diff(networks, nets_weights, comp_net):
+    plot_data = []
+    comparision_name = comp_net[0][:-3]+'_'+comp_net[1]
+    nets = []
+    for gen in generators:
+        net_name = gen[0][:-3]+'_'+gen[1]
+        nets.append(net_name)
+        group_data = []
+        diff, total_diff = weights_diff(nets_weights[comp_net], nets_weights[gen])
+        exp_order = []
+        for layer in diff.keys():
+            layer_name = layer.split('.')[0]
+            if comparision_name == net_name:
+                group_data.append(layer_name)
+            else:
+                group_data.append(diff[layer])
+            exp_order.append(layer_name)
+        plot_data.append(group_data)
+    plot_df = pd.DataFrame(plot_data, columns=exp_order)
+    plot_df = plot_df.T
+    plot_df.columns = nets
+    print(plot_df)
+    # plot grouped bar chart
+    plot_df.plot(x=comparision_name,
+            kind='bar',
+            stacked=False,
+            title=comparision_name
+            )
+    plt.ylabel('Layer mean abs difference')
+    plt.xlabel('Layer Name')
+    plt.legend(loc='right', title='Network')
+    save_file = 'results/weights_diff_layers_'+comparision_name+'.png'
     plt.savefig(save_file)
     print('Saved figure to: '+save_file)
 
@@ -69,4 +104,5 @@ if __name__ == '__main__':
     for generator in tqdm(generators, desc="Generators", leave=False):
         layers = get_results(ARGS, generator)
         nets_weights[generator] = layers
-    plot_avg_nets(generators, nets_weights, groupby='Generator')
+    # plot_avg_nets(generators, nets_weights, groupby='Generator')
+    plot_layers_diff(generators, nets_weights, generators[0])
