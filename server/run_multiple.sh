e69538fb6a9b5192de941b42a9193ca4cdb2c31d
matt clifford <matt.clifford@bristol.ac.uk>
2022-07-08 10:31:39 +0100

    dir

--- server/run_multiple.sh
+++ server/run_multiple.sh
@@ -7,17 +7,17 @@ job_name="MT"
 epochs=50
 
 lr="0.01"
-sbatch -t $time -J $job_name$lr -o $dir$lr'.out' -e $dir$lr'.err' --mem=$ram submit_job.sh python trainers/train_unet.py --batch_size 256 --epochs $epochs --dir /user/work/mc15445/summer-project --lr $lr
+sbatch -t $time -J $job_name$lr -o $dir$lr'.out' -e $dir$lr'.err' --mem=$ram server/submit_job.sh python trainers/train_unet.py --batch_size 256 --epochs $epochs --dir /user/work/mc15445/summer-project --lr $lr
 lr="0.001"
-sbatch -t $time -J $job_name$lr -o $dir$lr'.out' -e $dir$lr'.err' --mem=$ram submit_job.sh python trainers/train_unet.py --batch_size 256 --epochs $epochs --dir /user/work/mc15445/summer-project --lr $lr
+sbatch -t $time -J $job_name$lr -o $dir$lr'.out' -e $dir$lr'.err' --mem=$ram server/submit_job.sh python trainers/train_unet.py --batch_size 256 --epochs $epochs --dir /user/work/mc15445/summer-project --lr $lr
 lr="0.0001"
-sbatch -t $time -J $job_name$lr -o $dir$lr'.out' -e $dir$lr'.err' --mem=$ram submit_job.sh python trainers/train_unet.py --batch_size 256 --epochs $epochs --dir /user/work/mc15445/summer-project --lr $lr
+sbatch -t $time -J $job_name$lr -o $dir$lr'.out' -e $dir$lr'.err' --mem=$ram server/submit_job.sh python trainers/train_unet.py --batch_size 256 --epochs $epochs --dir /user/work/mc15445/summer-project --lr $lr
 lr="0.00001"
-sbatch -t $time -J $job_name$lr -o $dir$lr'.out' -e $dir$lr'.err' --mem=$ram submit_job.sh python trainers/train_unet.py --batch_size 256 --epochs $epochs --dir /user/work/mc15445/summer-project --lr $lr
+sbatch -t $time -J $job_name$lr -o $dir$lr'.out' -e $dir$lr'.err' --mem=$ram server/submit_job.sh python trainers/train_unet.py --batch_size 256 --epochs $epochs --dir /user/work/mc15445/summer-project --lr $lr
 lr="0.000001"
 
 
 # test out multiple gpu job
 ram="64G"
 lr="0.0001"
-sbatch -t $time -J $job_name$lr -o $dir$lr'.out' -e $dir$lr'.err' --mem=$ram submit_job_2.sh python trainers/train_unet.py --batch_size 512 --multi_GPU --epochs $epochs --dir /user/work/mc15445/summer-project --lr $lr
+sbatch -t $time -J $job_name$lr -o $dir$lr'.out' -e $dir$lr'.err' --mem=$ram server/submit_job_2.sh python trainers/train_unet.py --batch_size 512 --multi_GPU --epochs $epochs --dir /user/work/mc15445/summer-project --lr $lr
