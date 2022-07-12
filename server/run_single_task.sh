0e55dddc5fc94247e8400561a339f849a07fb0df
Matt Clifford <mc15445@my.bristol.ac.uk>
2022-07-12 17:06:44 +0100

    back to full

--- server/run_single_task.sh
+++ server/run_single_task.sh
@@ -9,15 +9,15 @@ epochs=500
 batch=64
 lr="0.001"
 
-# # train from scratch no no_gan
-# task="edge_2d tap"
-# sbatch -t $time -J $job_name -o $dir$lr'.out' -e $dir$lr'.err' --mem=$ram server/submit_job.sh python trainers/train_single_task.py --batch_size $batch --epochs $epochs --dir /user/work/mc15445/summer-project --lr $lr --task $task
-# task="edge_2d shear"
-# sbatch -t $time -J $job_name -o $dir$lr'.out' -e $dir$lr'.err' --mem=$ram server/submit_job.sh python trainers/train_single_task.py --batch_size $batch --epochs $epochs --dir /user/work/mc15445/summer-project --lr $lr --task $task
-# task="surface_3d tap"
-# sbatch -t $time -J $job_name -o $dir$lr'.out' -e $dir$lr'.err' --mem=$ram server/submit_job.sh python trainers/train_single_task.py --batch_size $batch --epochs $epochs --dir /user/work/mc15445/summer-project --lr $lr --task $task
-# task="surface_3d shear"
-# sbatch -t $time -J $job_name -o $dir$lr'.out' -e $dir$lr'.err' --mem=$ram server/submit_job.sh python trainers/train_single_task.py --batch_size $batch --epochs $epochs --dir /user/work/mc15445/summer-project --lr $lr --task $task
+# train from scratch no no_gan
+task="edge_2d tap"
+sbatch -t $time -J $job_name -o $dir$lr'.out' -e $dir$lr'.err' --mem=$ram server/submit_job.sh python trainers/train_single_task.py --batch_size $batch --epochs $epochs --dir /user/work/mc15445/summer-project --lr $lr --task $task
+task="edge_2d shear"
+sbatch -t $time -J $job_name -o $dir$lr'.out' -e $dir$lr'.err' --mem=$ram server/submit_job.sh python trainers/train_single_task.py --batch_size $batch --epochs $epochs --dir /user/work/mc15445/summer-project --lr $lr --task $task
+task="surface_3d tap"
+sbatch -t $time -J $job_name -o $dir$lr'.out' -e $dir$lr'.err' --mem=$ram server/submit_job.sh python trainers/train_single_task.py --batch_size $batch --epochs $epochs --dir /user/work/mc15445/summer-project --lr $lr --task $task
+task="surface_3d shear"
+sbatch -t $time -J $job_name -o $dir$lr'.out' -e $dir$lr'.err' --mem=$ram server/submit_job.sh python trainers/train_single_task.py --batch_size $batch --epochs $epochs --dir /user/work/mc15445/summer-project --lr $lr --task $task
 
 job_name="gans"
 # train from scratch GAN
