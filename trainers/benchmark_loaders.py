f23c3000cb3f652f6ca541cfa21527616a3d7573
Matt Clifford <mc15445@my.bristol.ac.uk>
2022-07-12 17:34:01 +0100

    use cpu

--- trainers/benchmark_loaders.py
+++ trainers/benchmark_loaders.py
@@ -26,6 +26,8 @@ if __name__ == '__main__':
     parser.add_argument("--batch_size",type=int,  default=64, help='batch size to load and train on')
     parser.add_argument("--prefetch",type=int,  default=1, help='prefetch data amount')
     parser.add_argument("--cores",type=int,  default=int(multiprocessing.cpu_count()/2), help='number of cpu cores to use')
+    parser.add_argument("--epochs",type=int,  default=5, help='epochs to use')
+    parser.add_argument("--use_cpu", default=False, action='store_true', help='force cpu use')
     ARGS = parser.parse_args()
 
 
@@ -33,12 +35,15 @@ if __name__ == '__main__':
 
     cores = int(multiprocessing.cpu_count()/2)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    if ARGS.use_cpu == True:
+        device = 'cpu'
     l1 = DataLoader(dataset_train_1,
                      batch_size=ARGS.batch_size,
                      shuffle=False,
                      num_workers=ARGS.cores,
                      prefetch_factor=ARGS.prefetch)
 
-    for step, sample in enumerate(tqdm(l1, desc="Data loader", leave=True)):
-        im_real = sample['real'].to(device=device, dtype=torch.float)
-        im_sim = sample['sim'].to(device=device, dtype=torch.float)
+    for epoch in tqdm(range(ARGS.epochs), desc="epochs", leave=True):
+        for step, sample in enumerate(tqdm(l1, desc="Data loader", leave=False)):
+            im_real = sample['real'].to(device=device, dtype=torch.float)
+            im_sim = sample['sim'].to(device=device, dtype=torch.float)
