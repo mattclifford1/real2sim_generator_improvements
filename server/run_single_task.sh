d8500fcc4f9bd41a714808b19ca2ebd75179d030
matt clifford <matt.clifford@bristol.ac.uk>
2022-07-13 12:30:22 +0100

    load data to ram

--- server/run_single_task.sh
+++ server/run_single_task.sh
@@ -12,21 +12,21 @@ lr="0.001"
 
 # train from scratch no no_gan
 task="edge_2d tap"
-sbatch -t $time -J $job_name -o $dir$lr'.out' -e $dir$lr'.err' --mem=$ram server/submit_job.sh python trainers/train_single_task.py --batch_size $batch --epochs $epochs --dir $dir --lr $lr --task $task
+sbatch -t $time -J $job_name -o $dir$lr'.out' -e $dir$lr'.err' --mem=$ram server/submit_job.sh python trainers/train_single_task.py --batch_size $batch --epochs $epochs --dir $dir --lr $lr --task $task --ram
 task="edge_2d shear"
-sbatch -t $time -J $job_name -o $dir$lr'.out' -e $dir$lr'.err' --mem=$ram server/submit_job.sh python trainers/train_single_task.py --batch_size $batch --epochs $epochs --dir $dir --lr $lr --task $task
+sbatch -t $time -J $job_name -o $dir$lr'.out' -e $dir$lr'.err' --mem=$ram server/submit_job.sh python trainers/train_single_task.py --batch_size $batch --epochs $epochs --dir $dir --lr $lr --task $task --ram
 task="surface_3d tap"
-sbatch -t $time -J $job_name -o $dir$lr'.out' -e $dir$lr'.err' --mem=$ram server/submit_job.sh python trainers/train_single_task.py --batch_size $batch --epochs $epochs --dir $dir --lr $lr --task $task
+sbatch -t $time -J $job_name -o $dir$lr'.out' -e $dir$lr'.err' --mem=$ram server/submit_job.sh python trainers/train_single_task.py --batch_size $batch --epochs $epochs --dir $dir --lr $lr --task $task --ram
 task="surface_3d shear"
-sbatch -t $time -J $job_name -o $dir$lr'.out' -e $dir$lr'.err' --mem=$ram server/submit_job.sh python trainers/train_single_task.py --batch_size $batch --epochs $epochs --dir $dir --lr $lr --task $task
+sbatch -t $time -J $job_name -o $dir$lr'.out' -e $dir$lr'.err' --mem=$ram server/submit_job.sh python trainers/train_single_task.py --batch_size $batch --epochs $epochs --dir $dir --lr $lr --task $task --ram
 
 job_name="gans"
 # train from scratch GAN
 task="edge_2d tap"
-sbatch -t $time -J $job_name -o $dir$lr'.out' -e $dir$lr'.err' --mem=$ram server/submit_job.sh python trainers/train_single_task.py --GAN --batch_size $batch --epochs $epochs --dir $dir --lr $lr --task $task
+sbatch -t $time -J $job_name -o $dir$lr'.out' -e $dir$lr'.err' --mem=$ram server/submit_job.sh python trainers/train_single_task.py --GAN --batch_size $batch --epochs $epochs --dir $dir --lr $lr --task $task --ram
 task="edge_2d shear"
-sbatch -t $time -J $job_name -o $dir$lr'.out' -e $dir$lr'.err' --mem=$ram server/submit_job.sh python trainers/train_single_task.py --GAN --batch_size $batch --epochs $epochs --dir $dir --lr $lr --task $task
+sbatch -t $time -J $job_name -o $dir$lr'.out' -e $dir$lr'.err' --mem=$ram server/submit_job.sh python trainers/train_single_task.py --GAN --batch_size $batch --epochs $epochs --dir $dir --lr $lr --task $task --ram
 task="surface_3d tap"
-sbatch -t $time -J $job_name -o $dir$lr'.out' -e $dir$lr'.err' --mem=$ram server/submit_job.sh python trainers/train_single_task.py --GAN --batch_size $batch --epochs $epochs --dir $dir --lr $lr --task $task
+sbatch -t $time -J $job_name -o $dir$lr'.out' -e $dir$lr'.err' --mem=$ram server/submit_job.sh python trainers/train_single_task.py --GAN --batch_size $batch --epochs $epochs --dir $dir --lr $lr --task $task --ram
 task="surface_3d shear"
-sbatch -t $time -J $job_name -o $dir$lr'.out' -e $dir$lr'.err' --mem=$ram server/submit_job.sh python trainers/train_single_task.py --GAN --batch_size $batch --epochs $epochs --dir $dir --lr $lr --task $task
+sbatch -t $time -J $job_name -o $dir$lr'.out' -e $dir$lr'.err' --mem=$ram server/submit_job.sh python trainers/train_single_task.py --GAN --batch_size $batch --epochs $epochs --dir $dir --lr $lr --task $task --ram
