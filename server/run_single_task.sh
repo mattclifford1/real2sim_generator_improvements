44930a748b1387caa60e69bf3b7198f87c7309d4
matt clifford <matt.clifford@bristol.ac.uk>
2022-07-13 13:11:03 +0100

    lower time

--- server/run_single_task.sh
+++ server/run_single_task.sh
@@ -2,14 +2,14 @@
 dir="server/outputs/"   # you will need to make outputs directory using 'mkdir outputs'
 mkdir $dir    # comment this out if directory already made
 ram="32G"        # change these
-time="0-16:00"
-job_name="no_gans"
 
 dir="/user/work/mc15445/summer-project"
-epochs=500
+epochs=250
 batch=64
 lr="0.001"
 
+job_name="no_gans"
+time="0-7:00"
 # train from scratch no no_gan
 task="edge_2d tap"
 sbatch -t $time -J $job_name -o $dir$lr'.out' -e $dir$lr'.err' --mem=$ram server/submit_job.sh python trainers/train_single_task.py --batch_size $batch --epochs $epochs --dir $dir --lr $lr --task $task --ram
@@ -21,6 +21,7 @@ task="surface_3d shear"
 sbatch -t $time -J $job_name -o $dir$lr'.out' -e $dir$lr'.err' --mem=$ram server/submit_job.sh python trainers/train_single_task.py --batch_size $batch --epochs $epochs --dir $dir --lr $lr --task $task --ram
 
 job_name="gans"
+time="0-7:00"
 # train from scratch GAN
 task="edge_2d tap"
 sbatch -t $time -J $job_name -o $dir$lr'.out' -e $dir$lr'.err' --mem=$ram server/submit_job.sh python trainers/train_single_task.py --GAN --batch_size $batch --epochs $epochs --dir $dir --lr $lr --task $task --ram
