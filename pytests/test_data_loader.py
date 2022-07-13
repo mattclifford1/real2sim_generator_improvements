0b40dfe8a8dc2f4284facadebfde1b5ae0da6a12
matt clifford <matt.clifford@bristol.ac.uk>
2022-07-13 12:04:04 +0100

    data loader tests

--- pytests/test_data_loader.py
+++ pytests/test_data_loader.py
@@ -20,12 +20,17 @@ class test_data(unittest.TestCase):
         self.dir = 'pytests'
         self.size = 128
         self.h = l.image_handler(base_dir=self.dir, size=self.size)
+        self.h_ram = l.image_handler(base_dir=self.dir, size=self.size, store_ram=True)
 
     def test_can_make_loader(self):
         assert isinstance(self.h, l.image_handler)
+        assert isinstance(self.h_ram, l.image_handler)
 
     def test_can_get_sample(self):
         sample = self.h[0]
+        assert isinstance(sample['real'], torch.Tensor)
+        assert isinstance(sample['sim'], torch.Tensor)
+        sample = self.h_ram[0]
         assert isinstance(sample['real'], torch.Tensor)
         assert isinstance(sample['sim'], torch.Tensor)
 
@@ -33,10 +38,25 @@ class test_data(unittest.TestCase):
         sample = self.h[0]
         assert sample['real'].shape == (1, self.size, self.size)
         assert sample['sim'].shape == (1, self.size, self.size)
+        sample = self.h_ram[0]
+        assert sample['real'].shape == (1, self.size, self.size)
+        assert sample['sim'].shape == (1, self.size, self.size)
 
     def test_accepts_torch_loader(self):
         batch_size=2
         loader = torch.utils.data.DataLoader(self.h,
+                          batch_size=batch_size,
+                          shuffle=False,
+                          num_workers=batch_size)
+        for i, batch in enumerate(loader):
+            if i > 1:
+                break
+        assert batch is not None
+        assert batch['real'].shape == (batch_size, 1, self.size, self.size)
+
+    def test_accepts_torch_loader_ram(self):
+        batch_size=2
+        loader = torch.utils.data.DataLoader(self.h_ram,
                           batch_size=batch_size,
                           shuffle=False,
                           num_workers=batch_size)
