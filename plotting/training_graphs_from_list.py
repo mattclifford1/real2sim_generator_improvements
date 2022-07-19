f5532468dcbad0b7dbbe6faa4af3b68d239793d5
matt clifford <matt.clifford@bristol.ac.uk>
2022-07-19 10:46:15 +0100

    server reduce datassize retrain

--- plotting/training_graphs_from_list.py
+++ plotting/training_graphs_from_list.py
@@ -13,25 +13,52 @@ if __name__ == '__main__':
     ARGS = parser.parse_args()
 
     # define the  label:filepath   to plot
-    pretrained = 'pretrained_edge_shear'
+    pretrained = 'pretrained_edge_tap'
+    train_routine = 'LR:0.0002_decay:0.1_BS:64'
+    data_limits = [0.01, 0.05, 0.1, 0.25, 0.5, 0.75, 0.9]
     curves_to_plot = {
-        # 'edge tap': os.path.join('edge_2d', 'tap', 'not_pretrained', 'no_ganLR:0.0002_decay:0.1_BS:64', 'run_0', 'training_stats.csv'),
-        # 'edge tap GAN': os.path.join('edge_2d', 'tap', 'not_pretrained', 'GAN_LR:0.0002_decay:0.1_BS:64', 'run_1', 'training_stats.csv'),
-        # 'edge shear': os.path.join('edge_2d', 'shear', 'not_pretrained', 'no_ganLR:0.0002_decay:0.1_BS:64', 'run_0', 'training_stats.csv'),
-        # 'edge shear GAN': os.path.join('edge_2d', 'shear', 'not_pretrained', 'GAN_LR:0.0002_decay:0.1_BS:64', 'run_1', 'training_stats.csv'),
-        # 'surface tap': os.path.join('surface_3d', 'tap', 'not_pretrained', 'no_ganLR:0.0002_decay:0.1_BS:64', 'run_0', 'training_stats.csv'),
-        # 'surface tap GAN': os.path.join('surface_3d', 'tap', 'not_pretrained', 'GAN_LR:0.0002_decay:0.1_BS:64', 'run_0', 'training_stats.csv'),
-        'surface shear': os.path.join('surface_3d', 'shear', 'not_pretrained', 'no_ganLR:0.0002_decay:0.1_BS:64', 'run_0', 'training_stats.csv'),
-        'surface shear GAN': os.path.join('surface_3d', 'shear', 'not_pretrained', 'GAN_LR:0.0002_decay:0.1_BS:64', 'run_0', 'training_stats.csv'),
+        # '''not pretrained - full data'''
+        # 'edge tap': os.path.join('edge_2d', 'tap', 'not_pretrained', 'no_gan'+train_routine, 'run_0', 'training_stats.csv'),
+        # 'edge tap GAN': os.path.join('edge_2d', 'tap', 'not_pretrained', 'GAN_'+train_routine, 'run_1', 'training_stats.csv'),
+        # 'edge shear': os.path.join('edge_2d', 'shear', 'not_pretrained', 'no_gan'+train_routine, 'run_0', 'training_stats.csv'),
+        # 'edge shear GAN': os.path.join('edge_2d', 'shear', 'not_pretrained', 'GAN_'+train_routine, 'run_1', 'training_stats.csv'),
+        # 'surface tap': os.path.join('surface_3d', 'tap', 'not_pretrained', 'no_gan'+train_routine, 'run_0', 'training_stats.csv'),
+        # 'surface tap GAN': os.path.join('surface_3d', 'tap', 'not_pretrained', 'GAN_'+train_routine, 'run_0', 'training_stats.csv'),
+        # 'surface shear': os.path.join('surface_3d', 'shear', 'not_pretrained', 'no_gan'+train_routine, 'run_0', 'training_stats.csv'),
+        # 'surface shear GAN': os.path.join('surface_3d', 'shear', 'not_pretrained', 'GAN_'+train_routine, 'run_0', 'training_stats.csv'),
 
-        # 'pretrained[ss] edge tap': os.path.join('edge_2d', 'tap', pretrained, 'no_ganLR:0.0002_decay:0.1_BS:64', 'run_0', 'training_stats.csv'),
-        # 'pretrained[ss] edge tap GAN': os.path.join('edge_2d', 'tap', pretrained, 'GAN_LR:0.0002_decay:0.1_BS:64', 'run_0', 'training_stats.csv'),
-        # 'pretrained[ss] edge shear': os.path.join('edge_2d', 'shear', pretrained, 'no_ganLR:0.0002_decay:0.1_BS:64', 'run_0', 'training_stats.csv'),
-        # 'pretrained[ss] edge shear GAN': os.path.join('edge_2d', 'shear', pretrained, 'GAN_LR:0.0002_decay:0.1_BS:64', 'run_0', 'training_stats.csv'),
-        # 'pretrained[ss] surface tap': os.path.join('surface_3d', 'tap', pretrained, 'no_ganLR:0.0002_decay:0.1_BS:64', 'run_0', 'training_stats.csv'),
-        # 'pretrained[ss] surface tap GAN': os.path.join('surface_3d', 'tap', pretrained, 'GAN_LR:0.0002_decay:0.1_BS:64', 'run_0', 'training_stats.csv'),
-        'pretrained[ss] surface shear': os.path.join('surface_3d', 'shear', pretrained, 'no_ganLR:0.0002_decay:0.1_BS:64', 'run_0', 'training_stats.csv'),
-        'pretrained[ss] surface shear GAN': os.path.join('surface_3d', 'shear', pretrained, 'GAN_LR:0.0002_decay:0.1_BS:64', 'run_0', 'training_stats.csv'),
+        # '''pretrained - full data'''
+        # 'pretrained[et] edge tap': os.path.join('edge_2d', 'tap', pretrained, 'no_gan'+train_routine, 'run_0', 'training_stats.csv'),
+        # 'pretrained[et] edge tap GAN': os.path.join('edge_2d', 'tap', pretrained, 'GAN_'+train_routine, 'run_0', 'training_stats.csv'),
+        # 'pretrained[et] edge shear': os.path.join('edge_2d', 'shear', pretrained, 'no_gan'+train_routine, 'run_0', 'training_stats.csv'),
+        # 'pretrained[et] edge shear GAN': os.path.join('edge_2d', 'shear', pretrained, 'GAN_'+train_routine, 'run_0', 'training_stats.csv'),
+        # 'pretrained[et] surface tap': os.path.join('surface_3d', 'tap', pretrained, 'no_gan'+train_routine, 'run_0', 'training_stats.csv'),
+        # 'pretrained[et] surface tap GAN': os.path.join('surface_3d', 'tap', pretrained, 'GAN_'+train_routine, 'run_0', 'training_stats.csv'),
+        # 'pretrained[et] surface shear': os.path.join('surface_3d', 'shear', pretrained, 'no_gan'+train_routine, 'run_0', 'training_stats.csv'),
+        # 'pretrained[et] surface shear GAN': os.path.join('surface_3d', 'shear', pretrained, 'GAN_'+train_routine, 'run_0', 'training_stats.csv'),
+
+        # '''not pretrained - limited data'''
+        # 'surface shear '+str(data_limits[0]): os.path.join('surface_3d', 'shear', 'not_pretrained', 'no_gan'+train_routine+'_DS:'+str(data_limits[0]), 'run_0', 'training_stats.csv'),
+        # 'surface shear '+str(data_limits[1]): os.path.join('surface_3d', 'shear', 'not_pretrained', 'no_gan'+train_routine+'_DS:'+str(data_limits[1]), 'run_0', 'training_stats.csv'),
+        # 'surface shear '+str(data_limits[2]): os.path.join('surface_3d', 'shear', 'not_pretrained', 'no_gan'+train_routine+'_DS:'+str(data_limits[2]), 'run_0', 'training_stats.csv'),
+        # 'surface shear '+str(data_limits[3]): os.path.join('surface_3d', 'shear', 'not_pretrained', 'no_gan'+train_routine+'_DS:'+str(data_limits[3]), 'run_0', 'training_stats.csv'),
+        # 'surface shear '+str(data_limits[4]): os.path.join('surface_3d', 'shear', 'not_pretrained', 'no_gan'+train_routine+'_DS:'+str(data_limits[4]), 'run_0', 'training_stats.csv'),
+        # 'surface shear '+str(data_limits[5]): os.path.join('surface_3d', 'shear', 'not_pretrained', 'no_gan'+train_routine+'_DS:'+str(data_limits[5]), 'run_0', 'training_stats.csv'),
+        # 'surface shear '+str(data_limits[6]): os.path.join('surface_3d', 'shear', 'not_pretrained', 'no_gan'+train_routine+'_DS:'+str(data_limits[6]), 'run_0', 'training_stats.csv'),
+        # 'surface shear 1': os.path.join('surface_3d', 'shear', 'not_pretrained', 'no_gan'+train_routine, 'run_0', 'training_stats.csv'),
+
+        # '''pretrained - limited data'''
+        '[et]surface shear '+str(data_limits[0]): os.path.join('surface_3d', 'shear', pretrained, 'no_gan'+train_routine+'_DS:'+str(data_limits[0]), 'run_0', 'training_stats.csv'),
+        '[et]surface shear '+str(data_limits[1]): os.path.join('surface_3d', 'shear', pretrained, 'no_gan'+train_routine+'_DS:'+str(data_limits[1]), 'run_0', 'training_stats.csv'),
+        '[et]surface shear '+str(data_limits[2]): os.path.join('surface_3d', 'shear', pretrained, 'no_gan'+train_routine+'_DS:'+str(data_limits[2]), 'run_0', 'training_stats.csv'),
+        '[et]surface shear '+str(data_limits[3]): os.path.join('surface_3d', 'shear', pretrained, 'no_gan'+train_routine+'_DS:'+str(data_limits[3]), 'run_0', 'training_stats.csv'),
+        # '[et]surface shear '+str(data_limits[4]): os.path.join('surface_3d', 'shear', pretrained, 'no_gan'+train_routine+'_DS:'+str(data_limits[4]), 'run_0', 'training_stats.csv'),
+        # '[et]surface shear '+str(data_limits[5]): os.path.join('surface_3d', 'shear', pretrained, 'no_gan'+train_routine+'_DS:'+str(data_limits[5]), 'run_0', 'training_stats.csv'),
+        # '[et]surface shear '+str(data_limits[6]): os.path.join('surface_3d', 'shear', pretrained, 'no_gan'+train_routine+'_DS:'+str(data_limits[6]), 'run_0', 'training_stats.csv'),
+        # '[et]surface shear 1': os.path.join('surface_3d', 'shear', pretrained, 'no_gan'+train_routine, 'run_0', 'training_stats.csv'),
+
+
+
     }
 
     cols = ['mean training loss', 'val MSE', 'val_SSIM']
