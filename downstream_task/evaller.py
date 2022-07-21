7e571674b2c630568bc4c15decbad2d51ab88a48
matt clifford <matt.clifford@bristol.ac.uk>
2022-07-21 22:17:10 +0100

    print debug

--- downstream_task/evaller.py
+++ downstream_task/evaller.py
@@ -20,6 +20,7 @@ def load_weights(model, weights_path):
     if not os.path.isfile(weights_path):
         # raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), weights_path)
         raise ValueError("Couldn't find network weights path: "+str(weights_path)+"\nMaybe you need to train first?")
+    print(device)
     model.load_state_dict(torch.load(weights_path, map_location=torch.device(device)))
 
 
