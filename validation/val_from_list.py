b5bc6dd192a3d8ed1cd1e65b7f71b77f14e62b06
Matt Clifford <mc15445@gpu02.bc4.acrc.priv>
2022-07-25 15:33:30 +0100

    bc4 results

--- validation/val_from_list.py
+++ validation/val_from_list.py
@@ -25,18 +25,18 @@ train_routine = 'LR:0.0002_decay:0.1_BS:64'
 data_limits = [0.01, 0.05, 0.1, 0.25, 0.5, 0.75, 0.9, 1.0]
 models = {
     # '''not pretrained - limited data'''
-    'surface shear '+str(data_limits[0]): os.path.join(base_dir, 'surface_3d', 'shear', 'not_pretrained', 'no_gan'+train_routine+'_DS:'+str(data_limits[0]), 'run_0', 'models', 'best_generator.pth'),
-    'surface shear '+str(data_limits[1]): os.path.join(base_dir, 'surface_3d', 'shear', 'not_pretrained', 'no_gan'+train_routine+'_DS:'+str(data_limits[1]), 'run_0', 'models', 'best_generator.pth'),
-    'surface shear '+str(data_limits[2]): os.path.join(base_dir, 'surface_3d', 'shear', 'not_pretrained', 'no_gan'+train_routine+'_DS:'+str(data_limits[2]), 'run_0', 'models', 'best_generator.pth'),
-    'surface shear '+str(data_limits[3]): os.path.join(base_dir, 'surface_3d', 'shear', 'not_pretrained', 'no_gan'+train_routine+'_DS:'+str(data_limits[3]), 'run_0', 'models', 'best_generator.pth'),
-    'surface shear '+str(data_limits[4]): os.path.join(base_dir, 'surface_3d', 'shear', 'not_pretrained', 'no_gan'+train_routine+'_DS:'+str(data_limits[4]), 'run_0', 'models', 'best_generator.pth'),
-    'surface shear '+str(data_limits[5]): os.path.join(base_dir, 'surface_3d', 'shear', 'not_pretrained', 'no_gan'+train_routine+'_DS:'+str(data_limits[5]), 'run_0', 'models', 'best_generator.pth'),
-    'surface shear '+str(data_limits[6]): os.path.join(base_dir, 'surface_3d', 'shear', 'not_pretrained', 'no_gan'+train_routine+'_DS:'+str(data_limits[6]), 'run_0', 'models', 'best_generator.pth'),
-    'surface shear '+str(data_limits[7]): os.path.join(base_dir, 'surface_3d', 'shear', 'not_pretrained', 'no_gan'+train_routine+'_DS:'+str(data_limits[7]), 'run_0', 'models', 'best_generator.pth'),
+    #'surface shear '+str(data_limits[0]): os.path.join(base_dir, 'surface_3d', 'shear', 'not_pretrained', 'no_gan'+train_routine+'_DS:'+str(data_limits[0]), 'run_0', 'models', 'best_generator.pth'),
+    #'surface shear '+str(data_limits[1]): os.path.join(base_dir, 'surface_3d', 'shear', 'not_pretrained', 'no_gan'+train_routine+'_DS:'+str(data_limits[1]), 'run_0', 'models', 'best_generator.pth'),
+    #'surface shear '+str(data_limits[2]): os.path.join(base_dir, 'surface_3d', 'shear', 'not_pretrained', 'no_gan'+train_routine+'_DS:'+str(data_limits[2]), 'run_0', 'models', 'best_generator.pth'),
+    #'surface shear '+str(data_limits[3]): os.path.join(base_dir, 'surface_3d', 'shear', 'not_pretrained', 'no_gan'+train_routine+'_DS:'+str(data_limits[3]), 'run_0', 'models', 'best_generator.pth'),
+    #'surface shear '+str(data_limits[4]): os.path.join(base_dir, 'surface_3d', 'shear', 'not_pretrained', 'no_gan'+train_routine+'_DS:'+str(data_limits[4]), 'run_0', 'models', 'best_generator.pth'),
+    #'surface shear '+str(data_limits[5]): os.path.join(base_dir, 'surface_3d', 'shear', 'not_pretrained', 'no_gan'+train_routine+'_DS:'+str(data_limits[5]), 'run_0', 'models', 'best_generator.pth'),
+    #'surface shear '+str(data_limits[6]): os.path.join(base_dir, 'surface_3d', 'shear', 'not_pretrained', 'no_gan'+train_routine+'_DS:'+str(data_limits[6]), 'run_0', 'models', 'best_generator.pth'),
+    #'surface shear '+str(data_limits[7]): os.path.join(base_dir, 'surface_3d', 'shear', 'not_pretrained', 'no_gan'+train_routine+'_DS:'+str(data_limits[7]), 'run_0', 'models', 'best_generator.pth'),
     #
     # # '''pretrained - limited data'''
-    '[pre-et]surface shear '+str(data_limits[0]): os.path.join(base_dir, 'surface_3d', 'shear', pretrained, 'no_gan'+train_routine+'_DS:'+str(data_limits[0]), 'run_0', 'models', 'best_generator.pth'),
-    '[pre-et]surface shear '+str(data_limits[1]): os.path.join(base_dir, 'surface_3d', 'shear', pretrained, 'no_gan'+train_routine+'_DS:'+str(data_limits[1]), 'run_0', 'models', 'best_generator.pth'),
+    #'[pre-et]surface shear '+str(data_limits[0]): os.path.join(base_dir, 'surface_3d', 'shear', pretrained, 'no_gan'+train_routine+'_DS:'+str(data_limits[0]), 'run_0', 'models', 'best_generator.pth'),
+    #'[pre-et]surface shear '+str(data_limits[1]): os.path.join(base_dir, 'surface_3d', 'shear', pretrained, 'no_gan'+train_routine+'_DS:'+str(data_limits[1]), 'run_0', 'models', 'best_generator.pth'),
     '[pre-et]surface shear '+str(data_limits[2]): os.path.join(base_dir, 'surface_3d', 'shear', pretrained, 'no_gan'+train_routine+'_DS:'+str(data_limits[2]), 'run_0', 'models', 'best_generator.pth'),
     '[pre-et]surface shear '+str(data_limits[3]): os.path.join(base_dir, 'surface_3d', 'shear', pretrained, 'no_gan'+train_routine+'_DS:'+str(data_limits[3]), 'run_0', 'models', 'best_generator.pth'),
     '[pre-et]surface shear '+str(data_limits[4]): os.path.join(base_dir, 'surface_3d', 'shear', pretrained, 'no_gan'+train_routine+'_DS:'+str(data_limits[4]), 'run_0', 'models', 'best_generator.pth'),
