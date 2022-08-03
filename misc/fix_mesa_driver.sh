cd0967cb1a26175c3227dc232c689b5d6cd9eae8
matt clifford <matt.clifford@bristol.ac.uk>
2022-08-03 12:01:12 +0100

    intel driver fix for Gui pyqt6

---
+++ misc/fix_mesa_driver.sh
@@ -0,0 +1,10 @@
+# fix for loading iris drivers on ubuntu when running python from anaconda
+# copies and links the correct libs
+
+ENV=gui_test
+cd ~/anaconda3/envs/$ENV/lib
+mkdir backup  # Create a new folder to keep the original libstdc++
+mv libstd* backup  # Put all libstdc++ files into the folder, including soft links
+cp /usr/lib/x86_64-linux-gnu/libstdc++.so.6  ./ # Copy the c++ dynamic link library of the system here
+ln -s libstdc++.so.6 libstdc++.so
+ln -s libstdc++.so.6 libstdc++.so.6.0.19
