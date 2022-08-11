278f844bc67f7a7de85769d4cf44e55c58656b59
matt clifford <matt.clifford@bristol.ac.uk>
2022-08-11 18:19:20 +0100

    plot average of runs

---
+++ plotting/training_graphs_avg.py
@@ -0,0 +1,64 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+from argparse import ArgumentParser
+import numpy as np
+
+'''run with eg:
+$ python plotting/training_graphs_from_list.py --dir ~/Downloads/matt/
+
+notes: surface_3d shear pose estimations MAES:
+            - real   ->   0.041
+            - sim    ->   0.026/ 0.021
+'''
+
+def get_avg_of_runs(dir, csv_file='training_stats.csv'):
+    Ys = []
+    for run in os.listdir(dir):
+        print(run)
+        path = os.path.join(dir, run, csv_file)
+        df = pd.read_csv(path)
+        Ys.append(df[cols[i]].values[1:])
+    x = df['epoch'].values[1:]
+    Ys = np.array(Ys)
+    return x, np.mean(Ys, axis=0), np.std(Ys, axis=0)
+    # return x, df[cols[i]].values[1:], np.std(Ys, axis=0)
+
+if __name__ == '__main__':
+    parser = ArgumentParser(description='Plot training graphs')
+    parser.add_argument("--dir", default=os.path.join(os.path.expanduser('~'), 'summer-project', 'models', 'sim2real', 'matt'), help='path to folder where training graphs are within')
+    ARGS = parser.parse_args()
+
+    # define the  label:filepath   to plot
+    pretrained = 'pretrained_edge_tap'
+    train_routine = 'LR:0.0002_decay:0.1_BS:64'
+    data_limits = [0.01, 0.05, 0.1, 0.25, 0.5, 0.75, 0.9, 1.0]
+    curves_to_plot = {
+        ## ones to plot
+        str(int(data_limits[7]*100))+'% Data': os.path.join('surface_3d', 'shear', 'not_pretrained', 'no_gan'+train_routine+'_DS:'+str(data_limits[7])),
+        # str(int(data_limits[2]*100))+'% Data [transferred net]': os.path.join('surface_3d', 'shear', pretrained, 'no_gan'+train_routine+'_DS:'+str(data_limits[2])),
+        #
+        # str(int(data_limits[2]*100))+'% Data': os.path.join('surface_3d', 'shear', 'not_pretrained', 'no_gan'+train_routine+'_DS:'+str(data_limits[2])),
+        # str(int(data_limits[7]*100))+'% Data [transferred net]': os.path.join('surface_3d', 'shear', pretrained, 'no_gan'+train_routine+'_DS:'+str(data_limits[7])),
+    }
+
+    cols = ['mean training loss', 'val MSE', 'val_SSIM', 'Downstream MAE']
+    cols = ['Downstream MAE']
+
+    fig, ax = plt.subplots(nrows=1, ncols=len(cols), figsize=(8,6))
+    i = 0
+    if cols[i] == 'Downstream MAE':
+        ax.plot(range(1,251), [0.041]*250, label='real pose net')
+    for key in curves_to_plot.keys():
+        dir = curves_to_plot[key]    # training run csv file of stats
+        dir = os.path.join(ARGS.dir, dir)
+        x, avg, std = get_avg_of_runs(dir)
+        ax.plot(x, avg, label=key)
+        ax.set_xlabel('Epoch')
+    ax.set_title(cols[i])
+    if cols[i] == 'Downstream MAE':
+        ax.set_ylabel('MAE')
+        ax.set_ylim(0.02, 0.3)
+        ax.set_title('Downstream Task Performance')
+    ax.legend()
+    plt.show()
