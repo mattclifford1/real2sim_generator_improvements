2a9edf44d79d996ce7f19aeff54bf1e406f537c1
matt clifford <matt.clifford@bristol.ac.uk>
2022-08-11 17:56:26 +0100

    notes

---
+++ experiments-todo.md
@@ -0,0 +1,22 @@
+# Experiments that need running still
+TODO: code:
+- get average and std of training/validation runs
+- plot of MAE for val set PoseNet
+  - real
+  - sim
+
+
+RUN and PLOT:
+- GAN vs noGAN
+  - from scratch
+  - transfer
+  - GAN on its own?
+
+
+
+IQA metrics:
+- image transformations vs metrics/downstream
+
+
+MISC:
+- number of parameters in gen/disc (for computational analysis)
--- misc/len_of_trained_models.py
+++ misc/len_of_trained_models.py
@@ -1,17 +1,19 @@
-'''
-make a data store locally of the models held on disk to easy access and reading of models
-'''
 import os
 from argparse import ArgumentParser
 import pandas as pd
 import sys; sys.path.append('..'); sys.path.append('.')
 
-def get_all_models_in_dir(dir, model_name='best_generator.pth'):
-    models = []
+def get_all_models_in_dir(dir, csv_file='training_stats.csv', expected_len=251):
     for dirpath, _, filenames in os.walk(dir):
         for f in filenames:
-            models.append(os.path.relpath(os.path.join(dirpath, f), dir))
-    return models
+            if f == csv_file:
+                path = os.path.join(dirpath, f)
+                df_len = len(pd.read_csv(path))
+                if df_len != expected_len:
+                    print(path)
+                    print(len(pd.read_csv(path)))
+                    print('')
+            # models.append(os.path.relpath(os.path.join(dirpath, f), dir))
 
 
 
@@ -24,5 +26,4 @@ if __name__ == '__main__':
     parser = ArgumentParser(description='Test data with GAN models')
     parser.add_argument("--dir", default=os.path.join('..', 'models', 'sim2real', 'matt'), help='path to folder models are held')
     ARGS = parser.parse_args()
-
     models = get_all_models_in_dir(ARGS.dir)
