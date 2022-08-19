b2b07aebde7f553d9333e405a28fcc18610a7d7d
matt clifford <matt.clifford@bristol.ac.uk>
2022-08-19 11:49:08 +0100

    fix script $

--- server/copy_training_stats.sh
+++ server/copy_training_stats.sh
@@ -3,7 +3,8 @@
 git pull
 # to copy on bc4 all of the training csvs but not models/images
 cd ~/storage/summer-project/models/sim2real/matt/
-$ find * -name "*.csv" -exec cp --parents \{\} ~/summer-project/real2sim_multitask/gan_models/training_csvs/ \;
+find * -name "*.csv" -exec cp --parents \{\} ~/summer-project/real2sim_multitask/gan_models/training_csvs/ \;
+cd -
 git add .
 git commit -m 'bc4 upload of training stats'
 git push
