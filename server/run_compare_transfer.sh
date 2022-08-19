c4ad940b941bc2c6dde58c6d4436fee2a5284647
matt clifford <matt.clifford@bristol.ac.uk>
2022-08-19 16:37:16 +0100

    run for edge 2d tap

--- server/run_compare_transfer.sh
+++ server/run_compare_transfer.sh
@@ -8,11 +8,14 @@ epochs=250
 batch=64
 lr="0.0002"
 
-task="surface_3d shear"
+task="edge_2d tap"
 
 job_name="pre_no_gans"
 time="0-8:00"
 data_size="1"
+
+# from scratch
+sbatch -t $time -J $job_name -o $dir$lr'.out' -e $dir$lr'.err' --mem=$ram server/submit_job.sh python trainers/train_single_task.py --GAN --batch_size $batch --epochs $epochs --dir $data_dir --lr $lr --task $task --ram --data_size $data_size --eval_downstream
 
 # what pretrained model to use
 model=/user/work/mc15445/summer-project/models/sim2real/alex/trained_gans/[edge_2d]/128x128_[tap]_250epochs/checkpoints/best_generator.pth
